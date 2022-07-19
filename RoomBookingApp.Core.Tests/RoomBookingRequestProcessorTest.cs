d7fa8c4efd32655dc54ea264f5c2ccbf3ffb3525
Jossic <jossic.lapierre@gmail.com>
2022-07-19 22:40:05 +0200

    Setup firsts classes

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RoomBookingApp.Core
+{
+	public class RoomBookingRequestProcessorTest
+	{
+		[Fact]
+		public void Should_return_room_booking_response_with_request_value()
+        {
+			var BookingRequest = new RoomBookingRequest
+			{
+				FullName = "Test Name",
+				Email = "test@mqil.com",
+				Date = new DateTimeOffset(DateTime.Now)
+			};
+
+			var processor = new RoomBookingRequestProcessor();
+
+			RoomBookingResult result = processor.BookRoom(BookingRequest);
+        }
+	}
+}
+
