980413c34ecf8afef71b236c6dd3c37e66d8a055
Jossic <jossic.lapierre@marvin-bot.com>
2022-08-31 14:00:57 +0200

    Refactore code

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,30 +1,32 @@
-﻿using Shouldly;
-namespace RoomBookingApp.Core
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+
+namespace RoomBookingApp.Core;
+
+public class RoomBookingRequestProcessorTest
 {
-    public class RoomBookingRequestProcessorTest
+    [Fact]
+    public void Should_return_room_booking_response_with_request_value()
     {
-        [Fact]
-        public void Should_return_room_booking_response_with_request_value()
+        // Arrange
+        var request = new RoomBookingRequest
         {
-            // Arrange
-            var request = new RoomBookingRequest
-            {
-                FullName = "Test Name",
-                Email = "test@mqil.com",
-                Date = new DateTime(2022, 08, 31)
-            };
+            FullName = "Test Name",
+            Email = "test@mqil.com",
+            Date = new DateTime(2022, 08, 31)
+        };
 
-            var processor = new RoomBookingRequestProcessor();
+        var processor = new RoomBookingRequestProcessor();
 
-            // Act
-            RoomBookingResult result = processor.BookRoom(request);
+        // Act
+        RoomBookingResult result = processor.BookRoom(request);
 
-            // Assert
-            result.ShouldNotBeNull();
-            request.FullName.ShouldBe(result.FullName);
-            request.Email.ShouldBe(result.Email);
-            request.Date.ShouldBe(result.Date);
-        }
+        // Assert
+        result.ShouldNotBeNull();
+        request.FullName.ShouldBe(result.FullName);
+        request.Email.ShouldBe(result.Email);
+        request.Date.ShouldBe(result.Date);
     }
 }
 
